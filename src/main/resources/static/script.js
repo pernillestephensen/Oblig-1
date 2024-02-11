9bb6382933ba8dded0a74f5e9110a435303c9d94
pernillestephensen <Pkstephensen@gmail.com>
2024-02-11 17:00:32 +0100

    Validering

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,30 +1,81 @@
 const billetter = [];
+let ut = "";
+
+function validerInput(inputId, feilmeldingId, feilmeldingTekst) {
+    const inputElement = document.getElementById(inputId);
+    const feilmeldingElement = $("#" + feilmeldingId);
+
+    if (!inputElement.checkValidity()) {
+        feilmeldingElement.html(feilmeldingTekst);
+        return false;
+    } else {
+        feilmeldingElement.html("");
+        return true;
+    }
+}
+
+function validerAntall() {
+    validerInput("antall", "feilantall", "Vennligst velg antall");
+}
+
+function validerFornavn() {
+    validerInput("fornavn", "feilfornavn", "Vennligst skriv inn fornavn");
+}
+
+function validerEtternavn() {
+    validerInput("etternavn", "feiletternavn", "Vennligst skriv inn etternavn");
+}
+
+function validerTelefonnummer() {
+    const telefonnummerRegex = /^\d{8}$/;
+    const telefonnummer = $("#telefonnummer").val();
+
+    if (!telefonnummer || telefonnummer.length !== 8) {
+        $("#feiltelefonnummer").html("Vennligst skriv inn et gyldig telefonnummer på 8 sifre");
+        return false;
+    }
+
+    if (!telefonnummerRegex.test(telefonnummer)) {
+        $("#feiltelefonnummer").html("Vennligst skriv inn et gyldig telefonnummer på 8 sifre");
+        return false;
+    } else {
+        $("#feiltelefonnummer").html("");
+        return true;
+    }
+}
+
+function validerEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return validerInput("email", "feilemail", "Vennligst skriv inn en gyldig e-postadresse") && emailRegex.test($("#email").val());
+}
 
 function registrer(){
-    const film = $("#film").val();
-    const antall = $("#antall").val();
-    const fornavn = $("#fornavn").val();
-    const etternavn = $("#etternavn").val();
-    const telefonnummer = $("#telefonnummer").val();
-    const email = $("#email").val();
+    if (validerAntall() && validerFornavn() && validerEtternavn() && validerTelefonnummer() && validerEmail()) {
+        const film = $("#film").val();
+        const antall = $("#antall").val();
+        const fornavn = $("#fornavn").val();
+        const etternavn = $("#etternavn").val();
+        const telefonnummer = $("#telefonnummer").val();
+        const email = $("#email").val();
 
-    const billett = {
-        film : film,
-        antall : antall,
-        fornavn : fornavn,
-        etternavn : etternavn,
-        telefonnummer : telefonnummer,
-        email : email
-    };
+        const billett = {
+            film : film,
+            antall : antall,
+            fornavn : fornavn,
+            etternavn : etternavn,
+            telefonnummer : telefonnummer,
+            email : email
+        };
 
-    billetter.push(billett);
-    $('input').val('');
-    $('select').val('');
-    visBilletter()
+        billetter.push(billett);
+        $('input').val('');
+        $('select').val('');
+        visBilletter();
+    }
 }
 
 function visBilletter(){
-    let ut = "<div class='row'>" +
+        ut = "<div class='row'>" +
         "<div class='col-md-2';'>Film</div><div class='col-md-1';>Antall</div><div class='col-md-1';'>Fornavn</div>" +
         "<div class='col-md-1';>Etternavn</div><div class='col-md-1';'>Telefon</div><div class='col-md-1';'>E-mail</div>" +
         "</div>" + "</br>";
@@ -40,5 +91,12 @@ function visBilletter(){
     }
     $("#billetter").html(ut);
 
+}
+
+function slett(){
+    billetter.length = 0;
+    visBilletter();
+    ut = " ";
+    $("#billetter").html(ut);
 }
 
